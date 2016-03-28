bbf9c0f83c9abaecd37e446fbd1440d66cbfa97d
Du Huanpeng <u74147@gmail.com>
2016-03-28 21:07:18 +0800

    Lisence: just pick MIT, simple
    Signed-off-by: Du Huanpeng <u74147@gmail.com>

--- printk.c
+++ printk.c
@@ -1,3 +1,25 @@
+/*
+ * Copyright (c) 2016 Du Huanpeng<u74147@gmail.com>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+ * OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
 #ifdef DEBUG
 #include <stdio.h>
 void puts_ll(const char *buf)
@@ -92,7 +114,7 @@ int printks(const char *fmt)
 	unsigned int flag;
 	int c;
 	const char *seek;
-	char format[8+2];
+	char format[8+3];
 	int i, j;
 
 
