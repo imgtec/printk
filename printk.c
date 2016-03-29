15b91c30c07fdf74de3cddadaf71d05ce1efeba7
Du Huanpeng <u74147@gmail.com>
2016-03-29 10:52:58 +0800

    printk: a smaller size of printk
    Signed-off-by: Du Huanpeng <u74147@gmail.com>

--- printk.c
+++ printk.c
@@ -23,7 +23,7 @@
 #ifdef DEBUG
 
 #include <stdio.h>
-#define printch(c) putchar(c);
+#define printch(c) putchar(c)
 
 #else
 
@@ -37,18 +37,32 @@ int printch(int ch)
 
 #include <stdarg.h>
 
-
-void printi(unsigned int i, int base, int cap)
+int prints(const char *str)
 {
-	char buf[32+1];
-	int j;
-	int mod;
+	int ch;
+	int i = 0;
 
-	buf[32] = 0;
-	j = 32;
+	while(ch=str[i]) {
+		i++;
+		printch(ch);
+		if(i>80) break;
+	}
+
+	return i;
+
+
+}
+
+int printi(char buf[], unsigned int i, int base, int cap)
+{
+	int j;
+	unsigned int mod;
 
 	if(cap) cap = 'A' - 10;
 	else cap = 'a' - 10;
+
+	buf[0] = 0;
+	j = 0;
 
 	do {
 		mod = i % base;
@@ -56,143 +70,63 @@ void printi(unsigned int i, int base, int cap)
 		if(mod < 10) {
 			buf[--j] = '0' + mod;
 		} else {
-			buf[--j] = mod + cap;
+			buf[--j] = cap + mod;
 		}
 	} while(i);
 
-	prints(&buf[j]);
+	return j;
 }
-
-
-void printh(unsigned int h, int cap)
-{
-	char buf[9];
-	unsigned int c;
-	int i;
-
-	if(cap) cap = 'A' - 10;
-	else cap = 'a' - 10;
-
-	for(i=0; i<8; i++) {
-		c = h >> 28;
-		#ifdef DEBUG
-//		printf("c[%u] ", c);
-		#endif
-		if(c < 10) buf[i] = c +'0';
-		else buf[i] = c + cap;
-
-		h <<= 4;
-	}
-	buf[i] = 0;
-
-	prints(buf);
-}
-
-
-int prints(const char *str)
-{
-	int i;
-	int c;
-
-	for(i=0; str[i]; i++) {
-		c = str[i];
-		printch(c);
-	}
-}
-
-int printki(const char *fmt, int num)
-{
-//	prints("[");
-	int i = 0;
-
-	while(fmt[i]) i++;
-	i = fmt[i-1];
-
-	if(i=='b') printi(num, 2, 0);
-	else if(i=='d') printi(num, 10, 0);
-	else if(i=='o') printi(num, 8, 0);
-	else printi(num, 16, 1);
-
-//	prints("]");
-}
-
-int printkh(const char *fmt, int num)
-{
-//	prints(fmt);
-//	prints("[");
-	printh(num, 0);
-//	prints("]");
-}
-
-int printkc(const char *fmt, int ch)
-{
-	prints(fmt);
-	prints("[");
-	printch(ch);
-	prints("]");
-}
-
-
-
-
 
 /*
  * second version of vprintk.
  */
 
-
 int vprintks(const char *fmt, va_list args)
 {
-	unsigned int flag;
 	int c;
-	char format[16];
-	int i, j;
+	int i, j, pi;
+	char printbuf[32 + 1];
 
+	j = 0;
+	i = 0;
 
-	flag = 0;
-
-	j = 0;		/* format[] index. */
-	i = 0;		/* index of fmt[]. */
-
-	while(c = fmt[i++]) {
-		if(j) {	/* format */
-			format[j++] = c;
-			format[j] = 0;
-			if      (c == 'c')
-				printkc(format, va_arg(args, int)), j=0;
-			else if (c == '%')
-				printkc(format, c),                 j=0;
-			else if (c == 'd')
-				printki(format, va_arg(args, int)), j=0;
-			else if (c == 'u')
-				printki(format, va_arg(args, int)), j=0;
-			else if (c == 'x')
-				printkh(format, va_arg(args, int)), j=0;
-			else if (c == 'X')
-				printki(format, va_arg(args, int)), j=0;
-			else if (c == 'p')
-				printkh(format, va_arg(args, int)), j=0;
-			else if (c == 'P')
-				printkh(format, va_arg(args, int)), j=0;
-			else if (c == 'b')
-				printki(format, va_arg(args, int)), j=0;
-			else if (c == 'B')
-				printki(format, va_arg(args, int)), j=0;
-			else if (c == 's') {
-				prints(format);
-				prints("[");
+	while(c = fmt[i]) {
+		if(j != i++) {	/* formating */
+			if (c == 'c') {
+				pi = sizeof(printbuf);
+				printbuf[--pi] = 0;
+				printbuf[--pi] = va_arg(args, int);
+			} else if (c == '%') {
+				while(j<i) printch(fmt[j++]);
+			} else if (c == 'd') {
+				pi = sizeof(printbuf) - 1;
+				pi += printi(printbuf+pi, va_arg(args, int), 10, 0);
+			} else if (c == 'x' || c == 'X') {
+				pi = sizeof(printbuf) - 1;
+				pi += printi(printbuf+pi, va_arg(args, int), 16, c=='X');
+			} else if (c == 'b') {
+				pi = sizeof(printbuf) - 1;
+				pi += printi(printbuf+pi, va_arg(args, int), 16, 0);
+			} else if (c == 's') {
 				prints(va_arg(args, const char *));
-				j=0;
-				prints("]");
+				j=i;
+			} else if (i-j>8) {
+				while(j<i) printch(fmt[j++]);
 			}
-			else if (j>8)
-				prints(format),                     j=0;
-		}
-		else {
-			if(c == '%') format[j++] = c;
-			else printch(c);
+
+			if(pi) {
+				prints(printbuf+pi);
+				pi = 0;
+				j  = i;
+			}
+		} else if (c != '%') {
+			printch(c);
+			j++;
 		}
 	}
+
+	while(j<i) printch(fmt[j++]);	/* error format at the end. */
+
 	va_end(args);
 }
 
@@ -206,34 +140,20 @@ int printks(const char *fmt, ...)
 #ifdef DEBUG
 int main(int argc, char *argv[])
 {
-	char name[]="Du Huanpeng";
-	const char *fmt = 
-"%d,,,,%x %1234567890%123$% %c1234X %12345d\n"
-"%b  %23B , s%Xsfa%%, %kdfsdf\n";
+	char format[] = "%099x" "%-99x" "%99lu" "%??c";
+	printks("char: [%c]\n", 'C');
+	printks("hex: [%x]\n", 0xABCD1234);
+	printks("dec: [%d]\n", 1234567890);
+	printks("bin: [%b]\n", 0x8421);
+	printks("string: [%s]\n", "String");
+	printks("100: [%%]\n", 100);
+	printks("error format: [%1234567890c]\n", 100);
+	printks("error format: [%ASDF]\n", 100);
+	printks("Left pad zero: [%08X]\n", 0xABCDEF00);
+	printks("Right pad zero:[%-8X]\n", 0xABCDEF11);
 
-	prints(fmt);
-	printks(fmt, 1, 2, 3, 4, 5, 6, 7, 8);
-
-	printks("%d", 123456789, 1234);
-	printks("My name is %s.\n", name);
-	printks("0x8421 in binary:[%b]\n", 0x8421, 11);
 
 	return 0;
 
-	printh(0x1234ABCD,  0); 
-	printh(0x1234ABCD,  1);
-	printh(~0,  1);
-	printh(0,  1);
-
-	printi(0, 2, 1);
-	printi(~0, 2, 1);
-
-	printi(0, 10, 1);
-	printi(~0, 10, 1);
-
-	printi(012345670, 8, 1);
-	printi(012345670, 8, 1);
-	printi(123456789, 10, 1);
-	printi(0, 2, 1);
 }
 #endif
